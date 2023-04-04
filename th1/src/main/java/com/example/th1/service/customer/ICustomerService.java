9f3b8ac0eceec73fb19ceed4e584ffaeb5357b3b
HoangTran2108 <119851922+HoangTran2108@users.noreply.github.com>
2023-04-05 00:48:43 +0700

    [Thực hành] Quản lý khách hàng sử dụng RESTful

---
+++ th1/src/main/java/com/example/th1/service/customer/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.example.th1.service.customer;
+
+import com.example.th1.model.Customer;
+import com.example.th1.service.IGeneralService;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
